7a394707436d98c3991fbc12ac153765b4ee5334
juehv <juehv@tk-jh-frankenstein.tk.informatik.tu-darmstadt.de>
2016-10-12 15:32:26 +0200

    Update Carelink Import

---
+++ src/de/jhit/openmediavault/app/preferences/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.jhit.openmediavault.app.preferences;
+
+/**
+ *
+ * @author juehv
+ */
+public final class Constants {
+    private Constants(){}
+    
+    public static final int CARELINK_CSV_EMPTY_LINES = 5;
+    public static final String[] CARELINK_CSV_HEADER = {" "};
+}
